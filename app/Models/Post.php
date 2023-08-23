524c64cf0f1befd16a16974e5d5d9153746348a4
salvomc <smcrwrk@gmail.com>
2023-08-23 09:50:59 +0200

    [FEAT] faker

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
+    protected $fillable = ['titele', 'content', 'slug'];
+
+    // una funzione standarta che modifica il titpo da Ciao mondo a Ciao-mondo 
+    public static function generateSlug($title)
+    {
+        return Str::slug($title, '-');
+    }
+    
     use HasFactory;
 }
--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Post;
+use Faker\Generator as Faker;
 
 class PostSeeder extends Seeder
 {
@@ -12,8 +14,15 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 1; $i <= 10; $i++)
+        {
+            $post = new Post();
+            $post->title = $faker->sentence(3);
+            $post->content = $faker->text(50);
+            $post->slug = $post->generateSlug($post->title);
+            $post->save();
+        }
     }
 }
