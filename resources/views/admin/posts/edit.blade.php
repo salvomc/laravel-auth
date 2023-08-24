f858547e56af2cd0afa9ca64c0124a36b5a32b52
salvomc <smcrwrk@gmail.com>
2023-08-24 16:14:35 +0200

    [FIX] edit crud

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -10,25 +10,22 @@
                 </div>
             </div>
             <div class="col-12 mb-5">
-                <form action=" {{ Route('admin.posts.update', $post) }} " method="POST">
+                <form action=" {{ Route('admin.posts.update', $post->id) }} " method="POST">
                     @csrf
                     @method('PUT')
                     <div class="form-group border p-4">
                         <div class="row">
                             <div class="col-12 my-2">
                                 <label class="control-label my-2">Title</label>
-                                <input type="text" name="title" id="title" placeholder="Inserisci il titolo"
-                                    class="form-control" value="{{ old('title') ?? $post->title }}" required>
+                                <input type="text" name="title" id="title" placeholder="Enter title" class="form-control" value="{{ old('title') ?? $post->title }}" required>
                             </div>
                             <div class="col-12 my-2">
                                 <label class="control-label my-2">Content</label>
-                                <textarea name="description" id="description" placeholder="Inserisci la descrizione" cols="30" rows="10"
-                                    class="form-control" required>{{ old('description') ?? $post->content }}</textarea>
+                                <textarea name="content" id="content" placeholder="Enter description" cols="30" rows="10" class="form-control" required>{{ old('content') ?? $post->content }}</textarea>
                             </div>
                             <div class="col-12 my-2">
                                 <label class="control-label my-2">Slug</label>
-                                <input type="text" name="thumb" id="thumb" placeholder="Inserisci la thumb"
-                                    class="form-control" value="{{ old('thumb') ?? $post->slug }}" required>
+                                <input type="text" name="thumb" id="thumb" placeholder="Enter thumb" class="form-control" value="{{ old('thumb') ?? $post->slug }}" required>
                             </div>
                             {{-- <div class="col-12 my-2">
                                             <label class="control-label my-2">Cover Image</label>
